59fe592d8ed8bf00d866c7f09caead627263cdf7
thisisstreamliner <thisisstreamliner@gmail.com>
2019-12-02 16:34:45 +0200

    Making the list scroll

--- lib/main.dart
+++ lib/main.dart
@@ -24,21 +24,23 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter App'),),
-      body: Column(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                elevation: 1,
-                child: Text('CHART'),
+      body: SingleChildScrollView(
+        child: Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  elevation: 1,
+                  child: Text('CHART'),
+                ),
               ),
-            ),
-            UserTransactions(),
-          ],
-        ),
+              UserTransactions(),
+            ],
+          ),
+      ),
     );
   }
 
