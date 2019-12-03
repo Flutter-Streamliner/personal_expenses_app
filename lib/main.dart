a2ad12f4ceb167867a956d2ffbb9b9d2b0af923f
thisisstreamliner <thisisstreamliner@gmail.com>
2019-12-03 16:58:17 +0200

    deriving recent transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses_app/models/transaction.dart';
+import 'package:personal_expenses_app/widgets/chart.dart';
 import 'package:personal_expenses_app/widgets/new_transaction.dart';
 import 'package:personal_expenses_app/widgets/transaction_list.dart';
 
@@ -69,7 +70,11 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((transaction){
+      return transaction.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList( );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -88,14 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
             //mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                width: double.infinity,
-                child: Card(
-                  color: Colors.blue,
-                  elevation: 1,
-                  child: Text('CHART'),
-                ),
-              ),
+              Chart(_recentTransactions),
               TransactionList(transactions: _userTransactions,),
             ],
           ),
