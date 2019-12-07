5fc693b1e950cdeb3761ff90b153916e92bf226e
Tiger <thisisstreamliner@gmail.com>
2019-12-07 15:01:36 +0200

    adaptive flat button widget fixes

--- lib/widgets/adaptive_flat_button.dart
+++ lib/widgets/adaptive_flat_button.dart
@@ -8,7 +8,7 @@ class AdaptiveFlatButton extends StatelessWidget {
   final String text;
   final Function handler;
 
-  AdaptiveFlatButton({this.text, this.handler})
+  AdaptiveFlatButton({this.text, this.handler});
 
   @override
   Widget build(BuildContext context) {
